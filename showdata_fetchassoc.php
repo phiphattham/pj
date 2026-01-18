1a0ab8d3dccb5df17f8d6c9bdc9a930851b2643c
phiphattham <phiphattham26092544@gmail.com>
2026-01-18 20:32:21 +0700

    test

--- showdata_fetchassoc.php
+++ showdata_fetchassoc.php
@@ -11,47 +11,47 @@ $result = mysqli_query($con,$sql);
 // 4 คำสั่งในการดึงข้อมูลรูปแบบ array
 $row = mysqli_fetch_assoc($result);
 
-print_r($row);
+// print_r($row);
 // 5 จะเป็นรูปแบบ array เหมือนกันแค่เป็นรูปแบบ key  
 // key ในที่นี้จะเอาชื่อ column มาอ้างอิง
-// echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
-// echo "ชื่อ = ".$row["emp_fname"]."<br>";
-// echo "สกุล = ".$row["emp_lname"]."<br>";
-// echo "เพศ = ".$row["emp_gender"]."<br>";
-// echo "ทักษะ = ".$row["emp_skills"]."<br>";
-// echo "<hr>";
+echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
+echo "ชื่อ = ".$row["emp_fname"]."<br>";
+echo "สกุล = ".$row["emp_lname"]."<br>";
+echo "เพศ = ".$row["emp_gender"]."<br>";
+echo "ทักษะ = ".$row["emp_skills"]."<br>";
+echo "<hr>";
 
-// $row = mysqli_fetch_assoc($result);
-// echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
-// echo "ชื่อ = ".$row["emp_fname"]."<br>";
-// echo "สกุล = ".$row["emp_lname"]."<br>";
-// echo "เพศ = ".$row["emp_gender"]."<br>";
-// echo "ทักษะ = ".$row["emp_skills"]."<br>";
-// echo "<hr>";
+$row = mysqli_fetch_assoc($result);
+echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
+echo "ชื่อ = ".$row["emp_fname"]."<br>";
+echo "สกุล = ".$row["emp_lname"]."<br>";
+echo "เพศ = ".$row["emp_gender"]."<br>";
+echo "ทักษะ = ".$row["emp_skills"]."<br>";
+echo "<hr>";
 
-// $row = mysqli_fetch_assoc($result);
-// echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
-// echo "ชื่อ = ".$row["emp_fname"]."<br>";
-// echo "สกุล = ".$row["emp_lname"]."<br>";
-// echo "เพศ = ".$row["emp_gender"]."<br>";
-// echo "ทักษะ = ".$row["emp_skills"]."<br>";
-// echo "<hr>";
+$row = mysqli_fetch_assoc($result);
+echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
+echo "ชื่อ = ".$row["emp_fname"]."<br>";
+echo "สกุล = ".$row["emp_lname"]."<br>";
+echo "เพศ = ".$row["emp_gender"]."<br>";
+echo "ทักษะ = ".$row["emp_skills"]."<br>";
+echo "<hr>";
 
-// $row = mysqli_fetch_assoc($result);
-// echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
-// echo "ชื่อ = ".$row["emp_fname"]."<br>";
-// echo "สกุล = ".$row["emp_lname"]."<br>";
-// echo "เพศ = ".$row["emp_gender"]."<br>";
-// echo "ทักษะ = ".$row["emp_skills"]."<br>";
-// echo "<hr>";
+$row = mysqli_fetch_assoc($result);
+echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
+echo "ชื่อ = ".$row["emp_fname"]."<br>";
+echo "สกุล = ".$row["emp_lname"]."<br>";
+echo "เพศ = ".$row["emp_gender"]."<br>";
+echo "ทักษะ = ".$row["emp_skills"]."<br>";
+echo "<hr>";
 
-// $row = mysqli_fetch_assoc($result);
-// echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
-// echo "ชื่อ = ".$row["emp_fname"]."<br>";
-// echo "สกุล = ".$row["emp_lname"]."<br>";
-// echo "เพศ = ".$row["emp_gender"]."<br>";
-// echo "ทักษะ = ".$row["emp_skills"]."<br>";
-// echo "<hr>";
+$row = mysqli_fetch_assoc($result);
+echo "รหัสพนักงาน = ".$row["emp_id"]."<br>";
+echo "ชื่อ = ".$row["emp_fname"]."<br>";
+echo "สกุล = ".$row["emp_lname"]."<br>";
+echo "เพศ = ".$row["emp_gender"]."<br>";
+echo "ทักษะ = ".$row["emp_skills"]."<br>";
+echo "<hr>";
 
 
 
